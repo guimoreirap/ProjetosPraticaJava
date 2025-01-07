e1edabc60c0d527b7712d8c1a2e6129a64eb8134
Guilherme Moreira <guimoreira.ads@gmail.com>
2025-01-07 10:59:38 -0300

    Saque de conta bancaria

---
+++ saque_conta_bancaria/model/exceptions/LimitException.java
@@ -0,0 +1,10 @@
+package model.exceptions;
+
+public class LimitException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+
+	public LimitException(String msg) {
+		super(msg);
+	}
+}
