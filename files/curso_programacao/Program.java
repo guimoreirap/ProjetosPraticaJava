270e0ef6250ae4723b6bfac87a355a86e661acbe
Guilherme Moreira <guimoreira.ads@gmail.com>
2025-01-08 15:00:42 -0300

    Trabalhando com file de forma otimizada

--- files/curso_programacao/Program.java
+++ files/curso_programacao/Program.java
@@ -1,29 +1,24 @@
 package curso_programacao;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
-		File file = new File("C:\\Estudos\\TXT Importantes\\CommitNoGit.txt");
-		Scanner sc = null;
-		
-		try {
-			sc = new Scanner (file);
-			while(sc.hasNextLine()) {
-				System.out.println(sc.nextLine());
+		String path = "C:\\Estudos\\TXT Importantes\\CommitNoGit.txt";
+		try(BufferedReader br = new BufferedReader(new FileReader(path))){
+			String line = br.readLine();
+			
+			while(line != null) {
+				System.out.println(line);
+				line = br.readLine();
 			}
-		} catch(Exception e) {
-			System.out.println(e.getMessage());
+		} catch (IOException e) {
 			e.printStackTrace();
-		} finally {
-			if(sc != null) {
-				sc.close();
-			}
 		}
-
 	}
-
 }
